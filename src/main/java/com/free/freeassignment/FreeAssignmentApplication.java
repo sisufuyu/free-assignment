532306e2d410324e09f085c3940fe91da51d044b
sisu.fuyu <sisu.fuyu@gmail.com>
2023-04-06 15:24:11 +0300

    set up project with Spring Initializr

---
+++ src/main/java/com/free/freeassignment/FreeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.free.freeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/free/freeassignment/FreeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free.freeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
